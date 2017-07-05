c92e5d4555f362e3c718ffe50f000fc466082723
Fabio Catunda Marreco <fabio.marreco@accenture.com>
2017-07-04 22:24:51 -0300

    Testes unitarios

--- tests/Marreco.Finance.Tests/Calendar.fs
+++ tests/Marreco.Finance.Tests/Calendar.fs
@@ -1,12 +1,22 @@
 namespace Marreco.Finance.Tests
 
+(*
+#r "bin\\debug\\FSharp.ProjectTemplate.dll"
+#r "bin\\debug\\Expecto.dll"
+*)
+
 open Expecto
+open FSharp.ProjectTemplate.Calendar
+open System
+
 module CalendarTest =
 
 
   [<Tests>]
   let tests =
-    testCase "yes" <| fun () ->
-      let subject = "Hello World"
-      Expect.equal subject "Hello world"
-                   "The strings should equal"
+    testCase "simple calendar brazil" <| fun () ->
+      let holidays = [DateTime(2017,06,01); DateTime(2017,06,30); DateTime(2017,07,01); DateTime(2017,07,04)]
+      let calendar = Calendar("brazil", holidays)
+      Expect.equal (calendar.NetworkDays (DateTime(2017,06,29)) (DateTime(2017,07,03))) 1<days> "Erro contagemm dias uteis 1"
+      Expect.equal (calendar.NetworkDays (DateTime(2017,06,29)) (DateTime(2017,07,10))) 5<days> "Erro contagemm dias uteis 2"
+      Expect.equal (calendar.NetworkDays (DateTime(2017,07,03)) (DateTime(2017,07,05))) 1<days> "Erro contagemm dias uteis 3"
