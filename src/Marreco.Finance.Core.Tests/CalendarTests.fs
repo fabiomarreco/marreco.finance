5ef8748aeecc1ba1d98aea1f08ddfe0e15c00edf
Marreco <fabio.marreco@accenture.com>
2017-09-25 20:57:42 -0300

    wip

--- src/Marreco.Finance.Core.Tests/CalendarTests.fs
+++ src/Marreco.Finance.Core.Tests/CalendarTests.fs
@@ -6,8 +6,6 @@ open FsCheck.Xunit
 open Marreco.Finance.Core
 open Calendar
 
-
-
 let simpleCalendarTestData = [|
     ((DateTime(2017,06,29)), (DateTime(2017,07,03)), 1<days>);
     ((DateTime(2017,06,29)), (DateTime(2017,07,10)), 2<days>);
@@ -22,7 +20,15 @@ let ``When Calculating networkdays small sample`` (startDate:DateTime, endDate:D
     let actual = calendar.NetworkDays startDate endDate 
     Assert.StrictEqual(dayCount, actual)
 
-    
+
+[<Fact>]
+let ``when calculating empty holidays no exception is thrown`` () = 
+    let calendar = Calendar("brazil", [])
+    let actual = calendar.NetworkDays (DateTime(2045,11,14)) (DateTime(2005,09,16))
+    let expected = -10477<days>
+    Assert.StrictEqual(expected, actual)
+
+
 
 [<Fact>]
 let ``when calculating networkdays workdays is greater or equal to networkdays`` () =
