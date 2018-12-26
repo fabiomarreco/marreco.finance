d450cbc6b8fbb388c38fc27eddba8e45488fa06a
Marreco <fabio.marreco@accenture.com>
2018-12-26 19:13:39 -0200

    WIP

--- src/Marreco.Finance.Core/DayCount.fs
+++ src/Marreco.Finance.Core/DayCount.fs
@@ -24,61 +24,33 @@ module Conventions =
                 {Y=y2; M=m2; D=d2} ) -> 
                 (y2 - y1) * 360<days> +  (m2 - m1) * 30<days> + (d2 - d1) * 1<days>
 
-        let countDaysBetween adjustment s e= (tokenizeDate s, tokenizeDate e) |> adjustment |> countDaysBetween
+        let rec bondBasis (startDate, endDate) = 
+            let d1 = min (startDate.D) 30
+            let d2 = if (d1 = 30 || d1 = 31) then min (endDate.D) 30 else endDate.D
+            let startDate' = {startDate with D = d1 }
+            let endDate' = {endDate with D = d2 }
+            countDaysBetween (startDate', endDate')
 
-        module BondBasis = 
-            let rec adjustDates (startDate, endDate) = 
-                let d1 = min (startDate.D) 30
-                let d2 = if (d1 = 30 || d1 = 31) then min (endDate.D) 30 else endDate.D
-                let startDate' = {startDate with D = d1 }
-                let endDate' = {endDate with D = d2 }
-                (startDate', endDate')
+        let rec US (tStart, tEnd) = 
+            match (tStart, tEnd) with
+            | (EndOfMonth 2, EndOfMonth 2)          -> US (tStart, {tEnd with D=30})
+            | (EndOfMonth 2, _ )                    -> US ( {tStart with D = 30}, tEnd)
+            | ({Y=_; M=_; D=30}, {Y=_; M=_; D=31 })
+            | ({Y=_; M=_; D=31}, {Y=_; M=_; D=31 }) -> US (tStart, {tEnd with D=30})
+            | ({Y=_; M=_; D=31}, _)                 -> US ( {tStart with D = 30}, tEnd)
+            | _                                     -> countDaysBetween (tStart, tEnd)
 
-            let countDays = countDaysBetween adjustDates
+        let rec ICMA (tStart, tEnd)  = 
+            match (tStart, tEnd) with
+            | ({Y=_; M=_; D=31}, _) -> ICMA ( {tStart with D = 30}, tEnd)
+            | (_, {Y=_; M=_; D=31}) -> ICMA ( tStart , { tEnd with D = 30})
+            | _                     -> countDaysBetween (tStart, tEnd)
 
-        module US = 
-            let rec adjustDates (startDate, endDate) = 
-                match (startDate, endDate) with
-                | (EndOfMonth 2, EndOfMonth 2) -> adjustDates (startDate, {endDate with D=30})
-                | (EndOfMonth 2, _ ) -> adjustDates ( {startDate with D = 30}, endDate)
-                | ({Y=_; M=_; D=30}, {Y=_; M=_; D=31 })
-                | ({Y=_; M=_; D=31}, {Y=_; M=_; D=31 }) -> adjustDates (startDate, {endDate with D=30})
-                | ({Y=_; M=_; D=31}, _) -> adjustDates ( {startDate with D = 30}, endDate)
-                | _ -> (startDate, endDate)
-
-            let countDays = countDaysBetween adjustDates
-
-        module 
-
-        
-        module private Adjustments = 
-            let rec bondBasis (startDate, endDate) = 
-                let d1 = min (startDate.D) 30
-                let d2 = if (d1 = 30 || d1 = 31) then min (endDate.D) 30 else endDate.D
-                let startDate' = {startDate with D = d1 }
-                let endDate' = {endDate with D = d2 }
-                (startDate', endDate')
-
-            let rec US (startDate, endDate) = 
-                match (startDate, endDate) with
-                | (EndOfMonth 2, EndOfMonth 2) -> US (startDate, {endDate with D=30})
-                | (EndOfMonth 2, _ ) -> US ( {startDate with D = 30}, endDate)
-                | ({Y=_; M=_; D=30}, {Y=_; M=_; D=31 })
-                | ({Y=_; M=_; D=31}, {Y=_; M=_; D=31 }) -> US (startDate, {endDate with D=30})
-                | ({Y=_; M=_; D=31}, _) -> US ( {startDate with D = 30}, endDate)
-                | _ -> (startDate, endDate)
-
-            let rec ICMA (startDate, endDate)  = 
-                match (startDate, endDate) with
-                | ({Y=_; M=_; D=31}, _) -> ICMA ( {startDate with D = 30}, endDate)
-                | (_, {Y=_; M=_; D=31}) -> ICMA ( startDate , { endDate with D = 30})
-                | _ -> (startDate, endDate)
-                
-            let rec ISDA (startDate, endDate)  = 
-                match (startDate, endDate) with
-                | (EndOfMonth _, _) -> ISDA ( {startDate with D = 30}, endDate)
-                | (_, EndOfMonth m) when m <> 2 -> ICMA ( startDate , { endDate with D = 30})
-                | _ -> (startDate, endDate)
+        let rec ISDA (tStart, tEnd)  = 
+            match (tStart, tEnd) with
+            | (EndOfMonth _, _)             -> ISDA ( {tStart with D = 30}, tEnd)
+            | (_, EndOfMonth m) when m <> 2 -> ISDA ( tStart , { tEnd with D = 30})
+            | _                             -> countDaysBetween (tStart, tEnd)
 
 
 
